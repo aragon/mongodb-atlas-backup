08a863ae8b9f12834c1292bba8a9df9711b2b8cf
jjavieralv <jjavieralvarezrodriguez@gmail.com>
2024-02-20 21:27:19 +0100

    first check

---
+++ scripts/mongodb_backup.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+#
+# Dump selected databases into a github artifacts encrypted
+#
+########
+# VARS #
+########
+
+MONGO_DEB='https://fastdl.mongodb.org/mongocli/mongocli_1.28.0_linux_x86_64.deb'
+
+### Variables must be defined in github
+#MONGO_DATABASES="db1,db2,db3"
+#MONGO_USER=""
+#MONGO_PASS=""
+#MONGO_DB_URL=""
+#BACKUPS_PASSWORD=""
+
+### Script variables
+ZIP_ROUTE_DEFAULT="dump/"
+ZIP_FILENAME_DEFAULT="../mongoatlas_backup.zip"
+
+###############
+# PREPARATION #
+###############
+
+function install_mongosh(){
+    wget "${MONGO_DEB}"
+    apt install ./https://fastdl.mongodb.org/mongocli/mongocli_1.28.0_linux_x86_64.deb
+}
+
+function install_dependencies(){
+    apt install wget jq zip
+}
+function convert_databases_into_array(){
+    IFS=',' read -r -a databases <<< "$MONGO_DATABASES"
+}
+
+function preparation(){
+    install_mongosh
+    install_dependencies
+    convert_databases_into_array
+}
+
+
+######################
+# MONGODB MANAGEMENT #
+######################
+
+
+# @param $1 database URL
+# @param $2 database name
+# @param $3 mongodb user
+# @param $4 mongodb password
+# 
+function list_all_collections_in_a_db(){
+    mongosh \
+    mongodb+srv://$1/$2 \
+    -u "$3" \
+    -p "$4" \
+    --eval 'db.runCommand({listCollections: 1.0,nameOnly: true})' \
+    --quiet \
+    --json=canonical \
+    |jq .cursor.firstBatch[].name \
+    |cut -d '"' -f2 \
+    >collections
+}
+
+# @param $1 database URL
+# @param $2 database name
+# @param $3 mongodb user
+# @param $4 mongodb password
+# @param $5 mongodb collection
+function mongoexport_collection(){
+    mongoexport \
+    --uri mongodb+srv://$1/$2 \
+    --collection $5 \
+    -u $3 \
+    -p $4 \
+    --ssl \
+    --type json \
+    --out $2_$5.json
+}
+
+# @param $1 database URL
+# @param $2 database name
+# @param $3 mongodb user
+# @param $4 mongodb password
+# @param $5 mongodb collection
+
+function mongodump_database(){
+    mongodump \
+    --uri=mongodb+srv://$1 \
+    -d $2 \
+    -u $3 \
+    -p $4 \
+    --ssl \
+    -o dump/$(cut -d '.' -f1 <<< $1) \
+    --gzip
+}
+
+# @param $1 database URL
+# @env $databases[] array with database name
+# @param $2 mongodb user
+# @param $3 mongodb password
+#
+function dump_all_databases(){
+    for db in "${databases[@]}";do
+        mongodump_database $1 $db $2 $3
+    done
+}
+
+#####################
+# GENERATE ARTIFACT #
+#####################
+
+# @param $1 password
+# @param $2 encrypted file name
+# @param $3 route to encrypt
+function generate_password_protected_zip(){
+    zip -e -r -P "$1" "$2" "$3"
+}
+
+########
+# MAIN #
+########
+function main(){
+    preparation
+    dump_all_databases ${MONGO_DB_URL} ${MONGO_USER} ${MONGO_PASS}
+    generate_password_protected_zip "${BACKUPS_PASSWORD}" "${ZIP_FILENAME_DEFAULT}" "${ZIP_ROUTE_DEFAULT}"
+}
+
+main
